60df219a2f52b98f7a3b0a098c0e5ac2c26615e7
Ramen-Nood1ez <55363734+Ramen-Nood1ez@users.noreply.github.com>
2022-01-01 17:09:39 -0600

    Added confirmation of logging in

--- app.js
+++ app.js
@@ -81,8 +81,8 @@ app.get('/loginpage', (req, res) => {
 app.post('/login', (req, res) => {
 	var user = req.query.username
 	var password = req.query.password
-	
-	res.send(AuthUser(user, password))
+
+	res.send("Logged in: " + AuthUser(user, password))
 })
 
 app.post('/createarticle', (req, res) => {
