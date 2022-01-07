133ca161368160a80effd041b85fbe5f47dcff45
Ramen-Nood1ez <55363734+Ramen-Nood1ez@users.noreply.github.com>
2022-01-06 21:45:37 -0600

    Fixed critical bug

--- app.js
+++ app.js
@@ -136,7 +136,7 @@ app.get('/documents', (req, res) => {
 })
 
 app.get('/loginpage', (req, res) => {
-	if (req.cookies["loggedin"] == )
+	//if (req.cookies["loggedin"] == )
 	res.sendFile(path.join(__dirname, "public/login.html"))
 })
 
