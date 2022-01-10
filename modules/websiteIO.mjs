92f4705673c164a5ec603cae3a854de28aad8fd8
Ramen-Nood1ez <55363734+Ramen-Nood1ez@users.noreply.github.com>
2022-01-09 20:25:24 -0600

    added file name

--- modules/websiteIO.mjs
+++ modules/websiteIO.mjs
@@ -1,11 +1,11 @@
 import { query } from './db.mjs';
 
-export function addPicture(creatorid, file, description = '', autouse = false, constr = null) {
+export function addPicture(creatorid, filename, file, description = '', autouse = false, constr = null) {
 	const creator = creatorid
 	const filepath = file
 	const desc = description
 
-	const sqlquery = "INSERT INTO `pictures` (`uploader`, `file`, `description`, `autouse`) VALUES (?, ?, ?, ?)"
+	const sqlquery = "INSERT INTO `pictures` (`uploader`, `filename`, `file`, `description`, `autouse`) VALUES (?, ?, ?, ?, ?)"
 
-	query(sqlquery, [creator, filepath, desc, autouse ? '1' : '0'], null, constr)
+	query(sqlquery, [creator, filename, filepath, desc, autouse ? '1' : '0'], null, constr)
 }
